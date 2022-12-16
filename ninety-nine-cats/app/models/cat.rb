a53fe53803d1004cb2606897d6b865f8e4031f0f
Janira Crispin <janiracrispin@gmail.com>
2022-12-16 17:12:04 -0500

    finished cats controller

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -13,16 +13,15 @@
 #
 class Cat < ApplicationRecord
     CAT_COLORS = ["blue", "orange", "grey"]
-    attr_reader :birth_date
     
-    validates :birth_date, :name
-    validates :color, inclusion: { in: CAT_COLORS }
-    validates :sex, inclusion: { in: ["M", "F"] }
-    validates :birth_date_cannot_be_future
+    validates :birth_date, :name, presence: true
+    validates :color, inclusion: { in: CAT_COLORS }, presence: true
+    validates :sex, inclusion: { in: ["M", "F"] }, presence: true
+    # validate :birth_date_cannot_be_future
 
-    def :birth_date_cannot_be_future
-        if birth_date > Date.today 
-            errors.add(:birth_date, "can't be in the future")
-        end
-    end
+    # def birth_date_cannot_be_future
+    #     if birth_date.present? && birth_date > Date.today 
+    #         errors.add(:birth_date, "can't be in the future")
+    #     end
+    # end
 end
